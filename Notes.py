2615aac05b3ae6ed494f1bfe61f176a52ba73ec4
Sinsinful <sin_1210@hotmail.com>
2023-04-16 13:50:16 +0100

    update for database

--- Notes.py
+++ Notes.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import os
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
 
 def register_user():
     username_info = username.get()
@@ -98,5 +101,31 @@ def main_screen():
     Button(text = "Register", height = "2", width = "30", command = register).pack()
 
     screen.mainloop()
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+connection = create_server_connection("localhost", "root", "Liverpool24")
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+create_database_query = "CREATE DATABASE notes"
+create_database(connection, create_database_query)
 
 main_screen()
