8264377b31b799fe27cd1127b6e08ef342e8dcdd
Sinsinful <sin_1210@hotmail.com>
2023-04-17 12:39:12 +0100

    update for notes page

--- Notes.py
+++ Notes.py
@@ -1,8 +1,14 @@
 from tkinter import *
+from tkinter import ttk
 import os
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
+
+
+db = "notes"
+pw = "Liverpool24"
+current_user = ""
 
 def register_user():
     username_info = username.get()
@@ -17,19 +23,30 @@ def register_user():
     password_entry.delete(0, END)
 
     Label(screen1, text= "Registration sucessful", fg = "green", font = ("calibri", 11)).pack()
+    create_user_table = """
+    CREATE TABLE {tab} (
+        date DATETIME PRIMARY KEY,
+        note VARCHAR(45) NOT NULL
+    );""".format(tab=username_info)
+    connection = create_db_connection("localhost", "root", pw, db) # Connect to the Database
+    execute_query(connection, create_user_table) # Execute our defined query
 
 def login_verify():
     username1 = username_verify.get()
     password1 = password_verify.get()
     username_entry1.delete(0, END)
     password_entry1.delete(0, END)
-
+    global current_user
+    current_user  = username1
     list_of_files = os.listdir()
     if username1 in list_of_files:
         file1 = open(username1, "r")
         verify = file1.read().splitlines()
         if password1 in verify:
             print("login sucessful")
+            screen2.destroy()
+            notes_screen()
+
         else:
             print("Password not reconised")
     else:
@@ -89,6 +106,26 @@ def login():
     Label(screen2, text = "").pack()
     Button(screen2, text = "Login", width = 10, height = 1, command = login_verify).pack()
 
+def notes_screen():
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.title("Current notes:")
+    screen3.geometry("600x500")
+
+    q1 = """ SELECT * FROM notes.{tab};""".format(tab = current_user )
+    connection = create_db_connection("localhost", "root", pw, db)
+    results = read_query(connection, q1)
+
+    i=0 
+    for entry in results:
+        for j in range(len(entry)):
+            e = Entry(screen3, width=30, fg='black') 
+            e.grid(row=i, column=j) 
+            e.insert(END, entry[j])
+        i=i+1
+    
+
+
 def main_screen():
     global screen
     screen = Tk()
@@ -99,8 +136,9 @@ def main_screen():
     Button(text = "Login", height = "2", width = "30", command = login).pack()
     Label(text = "").pack()
     Button(text = "Register", height = "2", width = "30", command = register).pack()
-
     screen.mainloop()
+
+
 def create_server_connection(host_name, user_name, user_password):
     connection = None
     try:
@@ -115,17 +153,42 @@ def create_server_connection(host_name, user_name, user_password):
 
     return connection
 
-connection = create_server_connection("localhost", "root", "Liverpool24")
-
-def create_database(connection, query):
+def execute_query(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
-        print("Database created successfully")
+        connection.commit()
+        print("Query successful")
     except Error as err:
         print(f"Error: '{err}'")
 
-create_database_query = "CREATE DATABASE notes"
-create_database(connection, create_database_query)
+def read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        print(f"Error: '{err}'")
 
+def create_db_connection(host_name, user_name, user_password, db_name):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+def add_note():
+    return 1
+
+connection = create_server_connection("localhost", "root", pw) # Connect to the Database
 main_screen()
